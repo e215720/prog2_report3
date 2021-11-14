88c68f3f6f2038210cabd6d692fa22b4680663ef
kazuki kinjo <e215720@ie.u-ryukyu.ac.jp>
2021-11-14 16:18:20 +0900

    4nd addの修正getter全ての名前の追加

--- app/src/main/java/jp/ac/uryukyu/ie/e215720/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215720/Hero.java
@@ -7,10 +7,10 @@ package jp.ac.uryukyu.ie.e215720;
  *  int attack; //敵の攻撃力
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
- *   String name; //指定されたモブの名前を返す
- *   int hitPoint; //指定されたモブのHPを返す
- *   int attack; //指定されたモブの攻撃力を返す
- *   boolean dead; //指定されたモブの生死状態を返す。true=死亡。
+ *   String getname; //指定されたモブの名前を返す
+ *   int gethitPoint; //指定されたモブのHPを返す
+ *   int getattack; //指定されたモブの攻撃力を返す
+ *   boolean getdead; //指定されたモブの生死状態を返す。true=死亡。
  */
 public class Hero {
     private String name;
