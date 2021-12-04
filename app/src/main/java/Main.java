97b5058792e8aa1592ca2d52199923c867506d78
kazuki kinjo <e215720@ie.u-ryukyu.ac.jp>
2021-12-04 19:54:08 +0900

    レベル2まで確かめて変更し忘れの変更

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,7 +2,7 @@ import jp.ac.uryukyu.ie.e215720.*;
 
 public class Main {
     public static void main(String[] args){
-        Warrior hero = new Warrior("勇者", 10, 5);
+        Hero hero = new Hero("勇者", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
@@ -11,7 +11,7 @@ public class Main {
         while( hero.isDead() == false && enemy.isDead() == false){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
-            hero.atteakWithWeponSkill(enemy);
+            hero.enemyAttack(enemy);
             enemy.heroAttack(hero);
         }
         System.out.println("戦闘終了");
